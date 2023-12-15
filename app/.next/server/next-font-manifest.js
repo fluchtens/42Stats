de31b2dccaac7122c94fb9e5a490963f9f8534fc
Cleazyy <cleazy2002@gmail.com>
2023-12-15 14:10:52 +0100

    add .next to gitignore

--- app/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/app/src/app/layout\":[\"static/media/3d9ea938b6afa941-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- app/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Azamfx4DbysYALadVwYNuWRrm1Mhwl3zT9VL3J9YXPs=\"\n}"
