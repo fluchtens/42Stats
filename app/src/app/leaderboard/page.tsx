a9d94e4d84003cedca1b93b14e59c8a999403c40
Cleazyy <cleazy2002@gmail.com>
2024-03-02 18:02:04 +0100

    update leaderboard

--- app/src/app/leaderboard/page.tsx
+++ app/src/app/leaderboard/page.tsx
@@ -6,15 +6,40 @@ export default async function Leaderboard() {
   return (
     <main className="p-6 flex-1">
       <div className="max-w-screen-lg m-auto flex-col flex justify-center items-center gap-2">
-        <table>
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <th>Login</th>
-              <th>Level</th>
-            </tr>
-          </thead>
-        </table>
+        {users && (
+          <table className="w-full">
+            <thead>
+              <tr className="text-base font-semibold">
+                <th className="text-left">#</th>
+                <th className="text-left">User</th>
+                <th className="text-right">Level</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr
+                  key={index}
+                  className={`${
+                    index !== users.length - 1
+                      ? "border-b border-slate-200 border-opacity-5"
+                      : ""
+                  } text-base font-normal`}
+                >
+                  <td className="py-4 text-left">{index + 1}</td>
+                  <td className="py-4 flex justify-start items-center gap-3 text-left">
+                    <img
+                      src={user.image}
+                      alt={user.image}
+                      className="w-10 h-10 rounded-full object-cover"
+                    />
+                    {user.login}
+                  </td>
+                  <td className="py-4 text-right">{user.level.toString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </main>
   );
