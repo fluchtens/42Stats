02e070b80c0d68ec819270f23b81d5bf93bd71bb
Cleazyy <cleazy2002@gmail.com>
2024-03-02 21:34:41 +0100

    init route api

---
+++ app/src/app/api/user/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { User } from "@prisma/client";
+import prisma from "@/libs/prisma";
+
+export async function getUserById(userId: number): Promise<User | null> {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      return null;
+    }
+    return user;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+
+  const userId = Number(url.searchParams.get("user_id"));
+  if (!userId) {
+    return NextResponse.json(
+      { message: "user_id cannot be empty." },
+      { status: 400 }
+    );
+  }
+
+  const user = await getUserById(userId);
+  if (!user) {
+    return NextResponse.json({ message: "User not found." }, { status: 404 });
+  }
+
+  return NextResponse.json({ user }, { status: 200 });
+}
