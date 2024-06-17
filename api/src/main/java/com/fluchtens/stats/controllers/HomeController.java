3fb475bd076bf107c5c7b7731bc37aa139439913
Cleazyy <cleazy2002@gmail.com>
2024-06-17 14:22:00 +0200

    fix home controller

--- api/src/main/java/com/fluchtens/stats/controllers/HomeController.java
+++ api/src/main/java/com/fluchtens/stats/controllers/HomeController.java
@@ -1,10 +1,10 @@
-package com.fluchtens.stats;
+package com.fluchtens.stats.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class HomeController {
   @GetMapping("/")
   public String sayHelloWorld() {
     return "Hello World!";
