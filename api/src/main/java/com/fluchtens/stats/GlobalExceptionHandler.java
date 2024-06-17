387496b5c481e2b905689fa77628cecc524b75d3
Cleazyy <cleazy2002@gmail.com>
2024-06-17 10:05:29 +0200

    create global exception handler

---
+++ api/src/main/java/com/fluchtens/stats/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.fluchtens.stats;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String errorMessage = "Invalid input: " + ex.getMessage();
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+}
