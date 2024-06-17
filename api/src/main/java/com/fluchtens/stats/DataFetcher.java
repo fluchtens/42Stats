27b00706f902545010c488305374d66cf70927a5
Cleazyy <cleazy2002@gmail.com>
2024-06-17 11:58:02 +0200

    use application.yml and init 42 auth

--- api/src/main/java/com/fluchtens/stats/DataFetcher.java
+++ api/src/main/java/com/fluchtens/stats/DataFetcher.java
@@ -1,211 +1,211 @@
-package com.fluchtens.stats;
+// package com.fluchtens.stats;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+// import java.io.BufferedReader;
+// import java.io.DataOutputStream;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.net.HttpURLConnection;
+// import java.net.URL;
+// import java.nio.charset.StandardCharsets;
+// import java.time.LocalDateTime;
+// import java.time.format.DateTimeFormatter;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+// import org.json.JSONArray;
+// import org.json.JSONObject;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.scheduling.annotation.Scheduled;
+// import org.springframework.stereotype.Component;
 
-import com.fluchtens.stats.models.FortyTwoCampus;
-import com.fluchtens.stats.models.FortyTwoUser;
-import com.fluchtens.stats.repositories.FortyTwoCampusRepository;
-import com.fluchtens.stats.repositories.FortyTwoUserRepository;
+// import com.fluchtens.stats.models.FortyTwoCampus;
+// import com.fluchtens.stats.models.FortyTwoUser;
+// import com.fluchtens.stats.repositories.FortyTwoCampusRepository;
+// import com.fluchtens.stats.repositories.FortyTwoUserRepository;
 
-@Component
-public class DataFetcher {
-  @Autowired
-  private FortyTwoCampusRepository campusRepository;
-  @Autowired
-  private FortyTwoUserRepository userRepository;
+// @Component
+// public class DataFetcher {
+//   @Autowired
+//   private FortyTwoCampusRepository campusRepository;
+//   @Autowired
+//   private FortyTwoUserRepository userRepository;
 
-  private String apiUrl = "https://api.intra.42.fr/v2";
-  private String accessToken;
+//   private String apiUrl = "https://api.intra.42.fr/v2";
+//   private String accessToken;
 
-  private String fetchAccessToken() {
-    try {
-      URL url = new URL("https://api.intra.42.fr/oauth/token");
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("POST");
-      connection.setRequestProperty("Content-Type", "application/json");
-      connection.setDoOutput(true);
+//   private String fetchAccessToken() {
+//     try {
+//       URL url = new URL("https://api.intra.42.fr/oauth/token");
+//       HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//       connection.setRequestMethod("POST");
+//       connection.setRequestProperty("Content-Type", "application/json");
+//       connection.setDoOutput(true);
   
-      String fortyTwoClient = "u-s4t2ud-53cc2c719851c28130a9c51e97a950c6c28dbbb5f074786a80c4a28de6568e2b";
-      String fortyTwoSecret = "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";
-      JSONObject requestBody = new JSONObject();
-      requestBody.put("grant_type", "client_credentials");
-      requestBody.put("client_id", fortyTwoClient);
-      requestBody.put("client_secret", fortyTwoSecret);
-      byte[] postData = requestBody.toString().getBytes(StandardCharsets.UTF_8);
+//       String fortyTwoClient = "u-s4t2ud-53cc2c719851c28130a9c51e97a950c6c28dbbb5f074786a80c4a28de6568e2b";
+//       String fortyTwoSecret = "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";
+//       JSONObject requestBody = new JSONObject();
+//       requestBody.put("grant_type", "client_credentials");
+//       requestBody.put("client_id", fortyTwoClient);
+//       requestBody.put("client_secret", fortyTwoSecret);
+//       byte[] postData = requestBody.toString().getBytes(StandardCharsets.UTF_8);
       
-      DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
-      dos.write(postData);
+//       DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+//       dos.write(postData);
 
-      if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-        return "";
-      }
+//       if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+//         return "";
+//       }
 
-      StringBuilder response = new StringBuilder();
-      BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-      String line;
-      while ((line = bf.readLine()) != null) {
-        response.append(line);
-      }
+//       StringBuilder response = new StringBuilder();
+//       BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//       String line;
+//       while ((line = bf.readLine()) != null) {
+//         response.append(line);
+//       }
   
-      String strResponse = response.toString();
-      JSONObject jsonResponse = new JSONObject(strResponse);
-      return jsonResponse.getString("access_token");
-    } catch (Exception e) {
-      System.err.println("[fetchAccessToken] catched exception" + e.getMessage());
-      return "";
-    }
-  }
+//       String strResponse = response.toString();
+//       JSONObject jsonResponse = new JSONObject(strResponse);
+//       return jsonResponse.getString("access_token");
+//     } catch (Exception e) {
+//       System.err.println("[fetchAccessToken] catched exception" + e.getMessage());
+//       return "";
+//     }
+//   }
 
-  private JSONArray fetchCampusesPage(int page) {
-    try {
-      String apiUrl = String.format("https://api.intra.42.fr/v2/campus?page=%d&?page[size]=100", page);
-      URL url = new URL(apiUrl);
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("GET");
-      connection.setRequestProperty("Content-Type", "application/json");
-      connection.setRequestProperty("Authorization", "Bearer " + this.accessToken);
+//   private JSONArray fetchCampusesPage(int page) {
+//     try {
+//       String apiUrl = String.format("https://api.intra.42.fr/v2/campus?page=%d&?page[size]=100", page);
+//       URL url = new URL(apiUrl);
+//       HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//       connection.setRequestMethod("GET");
+//       connection.setRequestProperty("Content-Type", "application/json");
+//       connection.setRequestProperty("Authorization", "Bearer " + this.accessToken);
 
-      if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-        System.err.println("[fetchCampusesPage] An error occurred while retrieving data, retry in 15 seconds...");
-        Thread.sleep(15 * 1000);
-        return this.fetchCampusesPage(page);
-      } else {
-        StringBuilder response = new StringBuilder();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while ((line = bf.readLine()) != null) {
-            response.append(line);
-        }
-        bf.close();
-        return new JSONArray(response.toString());
-      }
-    } catch (Exception e) {
-      System.err.println("[fetchCampusesPage] catched exception" + e.getMessage());
-      return new JSONArray();
-    }
-  }
+//       if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+//         System.err.println("[fetchCampusesPage] An error occurred while retrieving data, retry in 15 seconds...");
+//         Thread.sleep(15 * 1000);
+//         return this.fetchCampusesPage(page);
+//       } else {
+//         StringBuilder response = new StringBuilder();
+//         BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//         String line;
+//         while ((line = bf.readLine()) != null) {
+//             response.append(line);
+//         }
+//         bf.close();
+//         return new JSONArray(response.toString());
+//       }
+//     } catch (Exception e) {
+//       System.err.println("[fetchCampusesPage] catched exception" + e.getMessage());
+//       return new JSONArray();
+//     }
+//   }
 
-  private JSONArray fetchCampusUsersPage(int campusId, int page) {
-    try {
-      String apiUrl = String.format(this.apiUrl + "/cursus_users?filter[cursus_id]=21&filter[campus_id]=%s&page[number]=%d&page[size]=100", campusId, page);
-      URL url = new URL(apiUrl);
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("GET");
-      connection.setRequestProperty("Content-Type", "application/json");
-      connection.setRequestProperty("Authorization", "Bearer " + this.accessToken);
+//   private JSONArray fetchCampusUsersPage(int campusId, int page) {
+//     try {
+//       String apiUrl = String.format(this.apiUrl + "/cursus_users?filter[cursus_id]=21&filter[campus_id]=%s&page[number]=%d&page[size]=100", campusId, page);
+//       URL url = new URL(apiUrl);
+//       HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//       connection.setRequestMethod("GET");
+//       connection.setRequestProperty("Content-Type", "application/json");
+//       connection.setRequestProperty("Authorization", "Bearer " + this.accessToken);
 
-      if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-        System.err.println("[fetchCampusUsersPage] An error occurred while retrieving data, retry in 15 seconds...");
-        Thread.sleep(15 * 1000);
-        return this.fetchCampusUsersPage(campusId, page);
-      } else {
-        StringBuilder response = new StringBuilder();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while ((line = bf.readLine()) != null) {
-            response.append(line);
-        }
-        bf.close();
-        return new JSONArray(response.toString());
-      }
-    } catch (Exception e) {
-      System.err.println("[fetchCampusUsersPage] catched exception" + e.getMessage());
-      return new JSONArray();
-    }
-  }
+//       if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+//         System.err.println("[fetchCampusUsersPage] An error occurred while retrieving data, retry in 15 seconds...");
+//         Thread.sleep(15 * 1000);
+//         return this.fetchCampusUsersPage(campusId, page);
+//       } else {
+//         StringBuilder response = new StringBuilder();
+//         BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//         String line;
+//         while ((line = bf.readLine()) != null) {
+//             response.append(line);
+//         }
+//         bf.close();
+//         return new JSONArray(response.toString());
+//       }
+//     } catch (Exception e) {
+//       System.err.println("[fetchCampusUsersPage] catched exception" + e.getMessage());
+//       return new JSONArray();
+//     }
+//   }
 
-  private void fetchAllCampuses() {
-    int page = 1;
+//   private void fetchAllCampuses() {
+//     int page = 1;
 
-    while (true) {
-      JSONArray campusesJson = this.fetchCampusesPage(page);
-      if (campusesJson.length() <= 0) {
-        break;
-      }
-      for (int i = 0; i < campusesJson.length(); i++) {
-        JSONObject campusJson = campusesJson.getJSONObject(i);
-        FortyTwoCampus campus = new FortyTwoCampus();
-        campus.setId(campusJson.getInt("id"));
-        campus.setName(campusJson.getString("name"));
-        campus.setCountry(campusJson.getString("country"));
-        campus.setUserCount(campusJson.getInt("users_count"));
-        campusRepository.save(campus);
-        this.fetchAllCampusUsers(campus);
-      }
-      page++;
-    }
-  }
+//     while (true) {
+//       JSONArray campusesJson = this.fetchCampusesPage(page);
+//       if (campusesJson.length() <= 0) {
+//         break;
+//       }
+//       for (int i = 0; i < campusesJson.length(); i++) {
+//         JSONObject campusJson = campusesJson.getJSONObject(i);
+//         FortyTwoCampus campus = new FortyTwoCampus();
+//         campus.setId(campusJson.getInt("id"));
+//         campus.setName(campusJson.getString("name"));
+//         campus.setCountry(campusJson.getString("country"));
+//         campus.setUserCount(campusJson.getInt("users_count"));
+//         campusRepository.save(campus);
+//         this.fetchAllCampusUsers(campus);
+//       }
+//       page++;
+//     }
+//   }
 
-  private void fetchAllCampusUsers(FortyTwoCampus campus) {
-    int campusId = campus.getId();
-    int page = 1;
+//   private void fetchAllCampusUsers(FortyTwoCampus campus) {
+//     int campusId = campus.getId();
+//     int page = 1;
 
-    while (true) {
-      JSONArray usersJson = this.fetchCampusUsersPage(campusId, page);
-      if (usersJson.length() <= 0) {
-        break;
-      }
-      for (int i = 0; i < usersJson.length(); i++) {
-        JSONObject userJson = usersJson.getJSONObject(i);
-        FortyTwoUser user = new FortyTwoUser();
-        user.setId(userJson.getJSONObject("user").getInt("id"));
-        user.setEmail(userJson.getJSONObject("user").getString("email"));
-        user.setLogin(userJson.getJSONObject("user").getString("login"));
-        user.setFirstName(userJson.getJSONObject("user").getString("first_name"));
-        user.setLastName(userJson.getJSONObject("user").getString("last_name"));
-        if (!userJson.getJSONObject("user").getJSONObject("image").isNull("link")) {
-          user.setImage(userJson.getJSONObject("user").getJSONObject("image").getString("link"));
-        }
-        if (!userJson.getJSONObject("user").isNull("pool_month")) {
-          user.setPoolMonth(userJson.getJSONObject("user").getString("pool_month"));
-        }
-        if (!userJson.getJSONObject("user").isNull("pool_year")) {
-          user.setPoolYear(userJson.getJSONObject("user").getString("pool_year"));
-        }
-        user.setLevel(userJson.getDouble("level"));
-        user.setCampus(campus);
+//     while (true) {
+//       JSONArray usersJson = this.fetchCampusUsersPage(campusId, page);
+//       if (usersJson.length() <= 0) {
+//         break;
+//       }
+//       for (int i = 0; i < usersJson.length(); i++) {
+//         JSONObject userJson = usersJson.getJSONObject(i);
+//         FortyTwoUser user = new FortyTwoUser();
+//         user.setId(userJson.getJSONObject("user").getInt("id"));
+//         user.setEmail(userJson.getJSONObject("user").getString("email"));
+//         user.setLogin(userJson.getJSONObject("user").getString("login"));
+//         user.setFirstName(userJson.getJSONObject("user").getString("first_name"));
+//         user.setLastName(userJson.getJSONObject("user").getString("last_name"));
+//         if (!userJson.getJSONObject("user").getJSONObject("image").isNull("link")) {
+//           user.setImage(userJson.getJSONObject("user").getJSONObject("image").getString("link"));
+//         }
+//         if (!userJson.getJSONObject("user").isNull("pool_month")) {
+//           user.setPoolMonth(userJson.getJSONObject("user").getString("pool_month"));
+//         }
+//         if (!userJson.getJSONObject("user").isNull("pool_year")) {
+//           user.setPoolYear(userJson.getJSONObject("user").getString("pool_year"));
+//         }
+//         user.setLevel(userJson.getDouble("level"));
+//         user.setCampus(campus);
 
-        if (user.isValid()) {
-          userRepository.save(user);
-        }
-      }
-      page++;
-    }
-  }
+//         if (user.isValid()) {
+//           userRepository.save(user);
+//         }
+//       }
+//       page++;
+//     }
+//   }
 
-  @Scheduled(fixedRate = 24 * 60 * 60 * 1000)
-  public void fetchData() throws IOException {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    LocalDateTime startTime = LocalDateTime.now();
-    String formattedStartTime = startTime.format(formatter);
-    System.out.println("[" + formattedStartTime + "] Start of 42 api fetching...");
+//   @Scheduled(fixedRate = 24 * 60 * 60 * 1000)
+//   public void fetchData() throws IOException {
+//     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//     LocalDateTime startTime = LocalDateTime.now();
+//     String formattedStartTime = startTime.format(formatter);
+//     System.out.println("[" + formattedStartTime + "] Start of 42 api fetching...");
 
-    this.accessToken = this.fetchAccessToken();
-    if (this.accessToken.isEmpty()) {
-      return;
-    }
+//     this.accessToken = this.fetchAccessToken();
+//     if (this.accessToken.isEmpty()) {
+//       return;
+//     }
 
-    userRepository.deleteAll();
-    campusRepository.deleteAll();
-    this.fetchAllCampuses();
+//     userRepository.deleteAll();
+//     campusRepository.deleteAll();
+//     this.fetchAllCampuses();
 
-    LocalDateTime endTime = LocalDateTime.now();
-    String formattedEndTime = endTime.format(formatter);
-    System.out.println("[" + formattedEndTime + "] End of 42 api fetching.");
+//     LocalDateTime endTime = LocalDateTime.now();
+//     String formattedEndTime = endTime.format(formatter);
+//     System.out.println("[" + formattedEndTime + "] End of 42 api fetching.");
 
-  }
-}
+//   }
+// }
---
+++ api/src/main/java/com/fluchtens/stats/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.fluchtens.stats;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+            .authorizeHttpRequests(auth -> {
+              auth.requestMatchers("/").permitAll();
+              auth.anyRequest().authenticated();
+            })
+            .oauth2Login(withDefaults())
+            .formLogin(withDefaults())
+            .build();
+  }
+}
