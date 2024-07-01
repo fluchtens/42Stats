96844a54925bb2dc271424ea665228c487636a1b
Cleazyy <cleazy2002@gmail.com>
2024-07-01 13:21:59 +0200

    update stats route

--- client/src/app/stats/page.tsx
+++ client/src/app/stats/page.tsx
@@ -6,7 +6,6 @@ export default function Stats() {
   return (
     <main className="p-6 flex-1">
       <div className="max-w-screen-lg m-auto flex-col flex justify-center items-center gap-4">
-        <h1 className="text-base md:text-xl font-base">Statistics about all 42 campuses.</h1>
         <StatsCharts />
       </div>
     </main>
