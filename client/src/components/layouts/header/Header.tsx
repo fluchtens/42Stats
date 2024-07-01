97066d3b7278720c937c62897bc08331f8ad7c9d
Cleazyy <cleazy2002@gmail.com>
2024-07-01 16:08:34 +0200

    fix Header Links

--- client/src/components/layouts/header/Header.tsx
+++ client/src/components/layouts/header/Header.tsx
@@ -1,29 +1,14 @@
 "use server";
 
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Links } from "./Links";
 import { ProfileBtn } from "./NavLinks";
 import { ThemeToggle } from "./ThemeToggle";
 
 export const Header = async () => {
   return (
-    <header className="px-4 py-3 border-b border-slate-200 border-opacity-10">
+    <header className="px-4 py-3 border-b">
       <div className="max-w-screen-lg m-auto flex justify-between items-center">
-        <div className="flex justify-normal items-center gap-1">
-          <Link href="/" className="p-2">
-            <h1 className="text-lg md:text-xl font-medium">42Stats</h1>
-          </Link>
-          <Link href="/leaderboard">
-            <Button variant="ghost" size="default" className="text-zinc-200">
-              Leaderboard
-            </Button>
-          </Link>
-          <Link href="/stats">
-            <Button variant="ghost" size="default" className="text-zinc-200">
-              Stats
-            </Button>
-          </Link>
-        </div>
+        <Links />
         <div className="flex items-center gap-3">
           <ThemeToggle />
           <ProfileBtn />
---
+++ client/src/components/layouts/header/Links.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavLinkProps {
+  label: string;
+  link: string;
+  pathname: string;
+}
+
+const NavLink = ({ label, link, pathname }: NavLinkProps) => (
+  <Link
+    href={link}
+    className={`${
+      pathname === link ? "text-foreground" : "text-foreground/60"
+    } p-2 text-sm font-light hover:text-foreground/80 text-foreground/60 transition-colors`}
+  >
+    {label}
+  </Link>
+);
+
+export const Links = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex justify-normal items-center gap-0">
+      <Link href="/" className="p-2 text-lg md:text-xl font-medium">
+        42Stats
+      </Link>
+      <NavLink label="Leaderboard" link="/leaderboard" pathname={pathname} />
+      <NavLink label="Stats" link="/stats" pathname={pathname} />
+    </div>
+  );
+};
