3cd7527b1fd8d421343060da36781a8d29adcac5
Cleazyy <cleazy2002@gmail.com>
2024-07-03 10:54:31 +0200

    fix header mobile menu and dev docker compose

--- client/src/components/layouts/header/HeaderMobileMenu.tsx
+++ client/src/components/layouts/header/HeaderMobileMenu.tsx
@@ -25,10 +25,10 @@ export const HeaderMobileMenu = ({ pathname }: { pathname: string }) => (
   <Sheet>
     <SheetTrigger asChild className="block sm:hidden">
       <Button variant="transparent" size="icon" className="cursor-pointer" asChild>
-        <svg stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
-          <path d="M3 5H11" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-          <path d="M3 12H16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-          <path d="M3 19H21" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        <svg strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
+          <path d="M3 5H11" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+          <path d="M3 12H16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+          <path d="M3 19H21" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
         </svg>
       </Button>
     </SheetTrigger>
