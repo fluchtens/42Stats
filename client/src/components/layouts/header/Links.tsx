498a9bc95fd4de7f3361bbdaec70dc90d033de3a
Cleazyy <cleazy2002@gmail.com>
2024-07-08 16:00:40 +0200

    fix header nav

--- client/src/components/layouts/header/Links.tsx
+++ client/src/components/layouts/header/Links.tsx
@@ -13,7 +13,7 @@ interface NavLinkProps {
 const NavLink = ({ label, link, pathname }: NavLinkProps) => (
   <Link
     href={link}
-    className={`${pathname === link ? "text-foregound" : "text-foreground/60"} p-2 text-sm font-light hover:text-foreground/80 transition-colors`}
+    className={`${pathname === link ? "text-foregound" : "text-foreground/60"} py-2 text-base font-light hover:text-foreground/80 transition-colors`}
   >
     {label}
   </Link>
@@ -25,7 +25,7 @@ export const Links = () => {
   return (
     <>
       <HeaderMobileMenu pathname={pathname} />
-      <div className="hidden sm:flex sm:justify-normal sm:items-center sm:gap-0">
+      <div className="hidden sm:flex sm:justify-normal sm:items-center sm:gap-4">
         <Link href="/" className="py-2 text-lg md:text-xl font-medium">
           42Stats
         </Link>
