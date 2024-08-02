6c4bbcc11ee93d85e6b0dce8dbfe53d629c31304
Cleazyy <cleazy2002@gmail.com>
2024-08-02 11:17:41 +0200

    update next router

--- client/src/app/leaderboard/page.tsx
+++ client/src/app/leaderboard/page.tsx
@@ -1,16 +1,8 @@
-"use client";
+"use server";
 
-import { useAuth } from "@/hooks/useAuth";
-import { redirect } from "next/navigation";
 import { UserLeaderboard } from "./UserLeaderboard";
 
-export default function Leaderboard() {
-  const { user } = useAuth();
-
-  if (user === null) {
-    redirect("/");
-  }
-
+export default async function Leaderboard() {
   return (
     <main className="p-6 flex-1">
       <div className="max-w-screen-lg m-auto flex-col flex gap-4">
--- client/src/app/stats/page.tsx
+++ client/src/app/stats/page.tsx
@@ -1,17 +1,9 @@
-"use client";
+"use server";
 
-import { useAuth } from "@/hooks/useAuth";
-import { redirect } from "next/navigation";
 import { FortyTwoStats } from "./forty-two/FortyTwoStats";
 import { MainStats } from "./main/MainStats";
 
-export default function Stats() {
-  const { user } = useAuth();
-
-  if (user === null) {
-    redirect("/");
-  }
-
+export default async function Stats() {
   return (
     <main className="p-6 flex-1">
       <div className="max-w-screen-lg m-auto flex-col flex gap-10">
