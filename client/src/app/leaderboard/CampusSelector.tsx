4a7dda1b1564e48ee66ae468fc6f6403f793c57a
Cleazyy <cleazy2002@gmail.com>
2024-08-02 17:21:30 +0200

    update campus selector with search bar

--- client/src/app/leaderboard/CampusSelector.tsx
+++ client/src/app/leaderboard/CampusSelector.tsx
@@ -1,5 +1,11 @@
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectSeparator, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { SelectSeparator } from "@/components/ui/select";
+import { cn } from "@/lib/utils";
 import { Campus } from "@/types/campus.interface";
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 interface CampusSelectorProps {
   campuses: Campus[] | null;
@@ -7,12 +13,19 @@ interface CampusSelectorProps {
 }
 
 export const CampusSelector = ({ campuses, setCampusId }: CampusSelectorProps) => {
-  const handleSelect = (id: string) => {
-    setCampusId(Number(id));
-  };
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
 
   const groupCampusesByCountry = (campuses: Campus[]) => {
-    const groupedCampuses = campuses.reduce((groups, campus) => {
+    const sortedCampuses = campuses.sort((a, b) => {
+      if (a.country < b.country) return -1;
+      if (a.country > b.country) return 1;
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+
+    const groupedCampuses = sortedCampuses.reduce((groups, campus) => {
       const { country } = campus;
       if (!groups[country]) {
         groups[country] = [];
@@ -26,23 +39,42 @@ export const CampusSelector = ({ campuses, setCampusId }: CampusSelectorProps) =
   const groupedCampuses = campuses ? groupCampusesByCountry(campuses) : {};
 
   return (
-    <Select onValueChange={handleSelect}>
-      <SelectTrigger className="mt-1 w-60">
-        <SelectValue placeholder="Campus" />
-      </SelectTrigger>
-      <SelectContent>
-        {Object.entries(groupedCampuses).map(([country, campuses]) => (
-          <SelectGroup key={country}>
-            <SelectLabel>{country}</SelectLabel>
-            {campuses.map((campus) => (
-              <SelectItem key={campus.id} value={campus.id.toString()}>
-                {campus.name}
-              </SelectItem>
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button variant="outline" role="combobox" aria-expanded={open} className="mt-1 w-60 justify-between">
+          {value ? value : "Campus"}
+          <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0 w-60">
+        <Command>
+          <CommandInput placeholder="Search campus..." className="h-9" />
+          <CommandEmpty>No campus found.</CommandEmpty>
+          <CommandList>
+            {Object.entries(groupedCampuses).map(([country, campuses]) => (
+              <CommandGroup key={country} className="py-0">
+                <p className="px-2 py-1.5 text-sm font-semibold">{country}</p>
+                {campuses.map((campus) => (
+                  <CommandItem
+                    key={campus.id}
+                    value={campus.name}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue);
+                      setCampusId(campus.id);
+                      setOpen(false);
+                    }}
+                    className="py-1.5"
+                  >
+                    {campus.name}
+                    <CheckIcon className={cn("ml-auto h-4 w-4", value === campus.name ? "opacity-100" : "opacity-0")} />
+                  </CommandItem>
+                ))}
+                <SelectSeparator />
+              </CommandGroup>
             ))}
-            <SelectSeparator />
-          </SelectGroup>
-        ))}
-      </SelectContent>
-    </Select>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
   );
 };
