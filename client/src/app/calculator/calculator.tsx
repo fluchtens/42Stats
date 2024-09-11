8e47745cbd61fac513e50c1af3389cb4caff5fd3
Cleazyy <cleazy2002@gmail.com>
2024-09-11 12:09:20 +0200

    update calculator

--- client/src/app/calculator/calculator.tsx
+++ client/src/app/calculator/calculator.tsx
@@ -5,18 +5,19 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useAuth } from "@/hooks/useAuth";
 import { getProjects } from "@/services/project.service";
 import { Project } from "@/types/project.interface";
 import { useEffect, useState } from "react";
+import { FaCalculator } from "react-icons/fa6";
+import { ProjectSelector } from "./project-selector";
 
 export const Calculator = () => {
   const [level, setLevel] = useState<number>(0);
   const [projects, setProjects] = useState<Project[] | null>(null);
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
   const [grade, setGrade] = useState<number>(100);
-  const [newLevel, setNewLevel] = useState<number | null>(null);
+  const [newLevel, setNewLevel] = useState<number>(0);
   const { user } = useAuth();
 
   const handleLevelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,12 +31,16 @@ export const Calculator = () => {
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    if (!selectedProject) {
+      return;
+    }
+
     const xpRequired = [
       0, 462, 2688, 5885, 11777, 29217, 46255, 63559, 74340, 85483, 95000, 105630, 124446, 145782, 169932, 197316, 228354, 263508, 303366, 348516,
       399672, 457632, 523320, 597786, 682164, 777756, 886074, 1008798, 1147902, 1305486, 1484070,
     ];
     const currentLevel = level;
-    const projectXp = 24360;
+    const projectXp = selectedProject.difficulty;
     const projectGrade = grade;
 
     const currentLevelFloor = Math.floor(currentLevel);
@@ -67,54 +72,49 @@ export const Calculator = () => {
   }, []);
 
   return (
-    <div className="m-auto max-w-screen-sm w-full">
+    <>
       {user === null && <NotAuthAlert />}
       {user && (
         <>
           <h1 className="text-2xl md:text-3xl font-bold">XP Calculator</h1>
           <p className="text-sm md:text-lg font-light text-muted-foreground">Calculate your next XP level after turning in a project.</p>
-          <Card className="mt-6">
-            <form onSubmit={handleFormSubmit}>
-              <CardContent className="pt-6">
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="level">Level</Label>
-                    <Input id="level" type="number" step="0.01" placeholder="Your current level" value={level} onChange={handleLevelChange} />
+          <div className="mt-6 grid grid-cols-1 md:grid-cols-3 gap-4">
+            <Card className="md:col-span-2 bg-popover">
+              <form onSubmit={handleFormSubmit}>
+                <CardContent className="pt-6">
+                  <div className="grid items-center gap-4">
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="level">Level</Label>
+                      <Input id="level" type="number" step="0.01" placeholder="Your current level" value={level} onChange={handleLevelChange} />
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="project">Project</Label>
+                      <ProjectSelector projects={projects} selectedProject={selectedProject} setSelectedProject={setSelectedProject} />
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="grade">Grade</Label>
+                      <Input id="grade" type="number" placeholder="Your project grade" value={grade} onChange={handleGradeChange} />
+                    </div>
                   </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="project">Project</Label>
-                    <Select>
-                      <SelectTrigger id="project">
-                        <SelectValue placeholder="Select" />
-                      </SelectTrigger>
-                      <SelectContent position="popper">
-                        {projects?.map((project) => (
-                          <SelectItem key={project.id} value={project.id.toString()}>
-                            {project.name} ({project.difficulty} XP)
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="grade">Grade</Label>
-                    <Input id="grade" type="number" placeholder="Your project grade" value={grade} onChange={handleGradeChange} />
-                  </div>
+                </CardContent>
+                <CardFooter className="flex justify-end">
+                  <Button type="submit">Calculate</Button>
+                </CardFooter>
+              </form>
+            </Card>
+            <Card className="md:col-span-1 flex-col flex justify-center items-center bg-popover">
+              <CardContent className="pt-6 flex-col flex items-center gap-5">
+                <FaCalculator className="w-20 h-20" />
+                <div className="flex-col flex items-center">
+                  <span className="text-base font-medium text-muted-foreground">Level</span>
+                  <span className="text-2xl font-bold">{newLevel}</span>
+                  <span className="text-base font-medium text-green-600">+{(newLevel - level).toFixed(2)}</span>
                 </div>
               </CardContent>
-              <CardFooter className="flex justify-end">
-                <Button type="submit">Calculate</Button>
-              </CardFooter>
-            </form>
-          </Card>
-          <Card className="mt-6">
-            <CardContent className="pt-6">
-              <p className="text-2xl font-bold text-center">Result</p>
-              <p>{newLevel}</p>
-            </CardContent>
-          </Card>
+            </Card>
+          </div>
         </>
       )}
-    </div>
+    </>
   );
 };
---
+++ client/src/app/calculator/project-selector.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button";
+import { Command, CommandEmpty, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { Project } from "@/types/project.interface";
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+
+interface ProjectSelectorProps {
+  projects: Project[] | null;
+  selectedProject: Project | null;
+  setSelectedProject: (project: Project) => void;
+}
+
+export const ProjectSelector = ({ projects, selectedProject, setSelectedProject }: ProjectSelectorProps) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button variant="outline" className="justify-between">
+          {value ? value : "Select a project"}
+          <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <Command>
+        <PopoverContent className="p-0 popover-content-width-full">
+          <CommandInput placeholder="Search a project..." className="h-9" />
+          <CommandEmpty>No project found.</CommandEmpty>
+          <CommandList>
+            {projects &&
+              projects.map((project, index) => (
+                <CommandItem
+                  key={index}
+                  value={`${project.name} (${project.difficulty} XP)`}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue);
+                    setSelectedProject(project);
+                    setOpen(false);
+                  }}
+                  className="py-1.5 text-muted-foreground"
+                >
+                  {project.name} ({project.difficulty} XP)
+                  <CheckIcon
+                    className={cn("ml-auto h-4 w-4", value === `${project.name} (${project.difficulty} XP)` ? "opacity-100" : "opacity-0")}
+                  />
+                </CommandItem>
+              ))}
+          </CommandList>
+        </PopoverContent>
+      </Command>
+    </Popover>
+  );
+};
