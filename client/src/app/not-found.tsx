96da7d1916f348d3c3da1832fe3b48538ef6a896
Cleazyy <cleazy2002@gmail.com>
2024-10-12 13:07:53 +0200

    fix not found page

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -8,7 +8,7 @@ export default async function NotFound() {
     <main className="p-6 flex-1">
       <div className="m-auto mt-6 max-w-screen-xl flex flex-col justify-center items-center text-center">
         <h1 className="text-6xl font-bold text-primary">404</h1>
-        <p className="text-lg font-light text-muted-foreground">Oops! The page you're looking for doesn't exist.</p>
+        <p className="text-lg font-light text-muted-foreground">{"Oops! The page you're looking for doesn't exist."}</p>
         <Button variant="default" className="mt-3" asChild>
           <Link href="/">Back to home</Link>
         </Button>
