47e4e27c31ef2cab27957ab2039f3718a0b80396
Cleazyy <francoisluchtens2002@gmail.com>
2025-02-08 22:38:09 +0100

    remove old spring boot api

--- api/src/main/java/com/fluchtens/stats/campus/CampusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fluchtens.stats.campus;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CampusRepository extends JpaRepository<Campus, Integer> {}
